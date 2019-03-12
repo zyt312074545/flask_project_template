169e37ad3807cbb63f7f11256249255e90c31c20
zyt312074545 <zyt312074545@gmail.com>
2019-03-12 18:43:31 +0800

    add flask sqlalchemy

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 
 # third-package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Custom module
 from log import logger
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
 @app.route("/")
