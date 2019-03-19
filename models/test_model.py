137a45579bfab39c354889693b69018632e4d877
zyt312074545 <zyt312074545@gmail.com>
2019-03-19 23:22:38 +0800

    add password hash method

--- models/test_model.py
+++ models/test_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from models.base_model import BaseModel
 
@@ -7,4 +8,12 @@ class TestModel(BaseModel):
     __tablename__ = "test"
 
     username = Column(String(64), server_default="", comment="用户名称")
-    password = Column(String(64), server_default="", comment="密码")
+
+    # password_hash = method$salt$hash
+    password_hash = Column(String(128), server_default="", comment="密码")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
